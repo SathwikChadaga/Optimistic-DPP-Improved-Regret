c2bb5e273badbe8129d6b55d6b9a8184bf531930
SathwikChadaga <chadagasathwik@gmail.com>
2025-01-23 12:17:47 -0500

    some file restructuring to avoid redundant code

--- regret/utils/experiment.py
+++ regret/utils/experiment.py
@@ -1,5 +1,4 @@
 import numpy as np
-import utils.misc as mutil
 import utils.network as qnet
 import utils.policies as polc
 
@@ -34,7 +33,7 @@ def run_experiment(simulation_params, custom_seed = None, queueing_network = Non
 
         # estimate edge costs from previous observation
         estimated_edge_costs = queueing_network.edge_cost_means - np.sqrt(beta*np.log((queueing_network.tt+1)/delta)/queueing_network.edge_num_pulls)
-        estimated_cost_matrix = mutil.prepare_cost_matrix(queueing_network.node_edge_adjacency, estimated_edge_costs)
+        estimated_cost_matrix = prepare_cost_matrix(queueing_network.node_edge_adjacency, estimated_edge_costs)
 
         # get planned transmissions from the policy
         planned_edge_rates = polc.max_weight_policy(queue_state, queueing_network.node_edge_adjacency, estimated_cost_matrix, queueing_network.edge_capacities, nu)
@@ -56,3 +55,48 @@ def calculate_total_costs(queueing_network, cost_type = 'planned'):
     backlog_cost_at_T = backlog_at_T*np.sum(queueing_network.true_edge_costs) # C_B = sum_{ij} c_{ij}
 
     return tran_cost_till_T, backlog_cost_at_T
+
+# class to hold all simulaiton parameters
+class SimulationParameters:
+    def __init__(self, 
+                 node_edge_adjacency, 
+                 true_edge_costs, edge_capacities, 
+                 source_node, destination_node, 
+                 noise_variance, noise_distribution,
+                 arrival_rate, 
+                 N_runs, T_horizon, 
+                 beta, delta, nu):
+        self.N_runs              = N_runs
+        self.T_horizon           = T_horizon
+        self.arrival_rate        = arrival_rate
+        self.noise_variance      = noise_variance
+        self.noise_distribution  = noise_distribution
+        self.nu                  = nu
+        self.beta                = beta         
+        self.delta               = delta
+        self.node_edge_adjacency = node_edge_adjacency
+        self.source_node         = source_node
+        self.destination_node    = destination_node
+        self.true_edge_costs     = true_edge_costs
+        self.edge_capacities     = edge_capacities
+
+# prepares node to edge adjacency
+def prepare_adjacency(edges, N_nodes):
+    N_edges = len(edges)
+    node_edge_adjacency = np.zeros([N_nodes, N_edges]) # node_edge_adjacency_(v,e) = {-1 if e = Out(v), 1 if e = In(v), 0 otherwise}
+    for ll, vv in enumerate(edges):
+        node_edge_adjacency[vv[0], ll] = -1
+        node_edge_adjacency[vv[1], ll] = 1
+
+    return node_edge_adjacency
+
+# converts cost array into matrix
+def prepare_cost_matrix(node_edge_adjacency, costs):
+    if(len(costs.shape) > 1): 
+        cost_matrix = np.expand_dims(node_edge_adjacency == -1, axis=0)*costs[:, np.newaxis, :] + 0.0
+        cost_matrix[:, node_edge_adjacency != -1] = np.inf
+
+    else: 
+        cost_matrix = (node_edge_adjacency == -1)*costs + 0.0
+        cost_matrix[node_edge_adjacency != -1] = np.inf
+    return cost_matrix
--- regret/utils/plotter.py
+++ regret/utils/plotter.py
@@ -1,19 +1,34 @@
 import numpy as np
 from scipy.optimize import lsq_linear  
+import matplotlib.pyplot as plt
+import networkx as nx
 
-# low pass filter to make plots smoother 
-def windowed_average(input_array, window_size = 15):
-    output_array = np.convolve(input_array, np.ones(window_size)/window_size, mode='valid')
-    return np.append(input_array[0], output_array) # append used to match input and output size
+# plots the network topology
+def visualize_network(edges_list, N_nodes):
+    # add edges
+    G = nx.DiGraph()
+    for edge_ii in range(len(edges_list)):
+        G.add_edge(edges_list[edge_ii][0]+1, edges_list[edge_ii][1]+1)  
+        
+    # relabel nodes
+    mapping = {v+1 : v for v in range(N_nodes)}
+    G = nx.relabel_nodes(G, mapping)
 
-# fit curve to O(T^{2/3})
+    # Visualize the network
+    pos = nx.spectral_layout(G) # pos = nx.spring_layout(G) 
+    nx.draw_networkx(G, pos, with_labels=True)
+
+    plt.title("Network Visualization")
+    plt.show()
+
+# fit curve to O(T^{1/2})
 def fit_regret_curve(T_horizon_list, dpop_regret, start_index = 0):
-    # create T^{2/3} for given values of T
+    # create T^{1/2} for given values of T
     X = np.ones([T_horizon_list.shape[0], 2])
-    X[:,1] = T_horizon_list**(2/3) # (T_horizon_list**(1/2))*np.log(T_horizon_list)
+    X[:,1] = (T_horizon_list**(1/2))*np.log(T_horizon_list)
     bound_constraints = ([-np.inf,0], [np.inf,np.inf])
 
-    # fit given regret to O(T^{2/3})
+    # fit given regret to O(T^{1/2})
     regret_fit_dpop = lsq_linear(X[start_index:,:], dpop_regret[start_index:], bounds=bound_constraints)
     theoretical_dpop_regret = X@regret_fit_dpop.x
 
@@ -54,46 +69,4 @@ def plot_regret_curve(ax, arrival_rate, noise_variance_list, show_theoretical, s
     order.reverse()
     ax.legend([handles[idx] for idx in order],[labels[idx] for idx in order], loc='upper left')
     
-    ax.grid()
-
-# function to plot total queue backlogs
-def plot_backlog_curve(ax, unknownT_backlog_at_tt, knownT_backlog_at_tt, oracle_backlog_at_tt, label_font_size):
-    ax.plot(unknownT_backlog_at_tt, color = 'C0', label = 'DPOP (doubling)')
-    ax.plot(knownT_backlog_at_tt, color = 'C1', label = 'DPOP (given T)')
-    ax.plot(oracle_backlog_at_tt, color = 'C2', label = 'Oracle policy')
-
-    # show values in scientific notation and show exponent near axes
-    ax.set_xticks(ticks=1000*np.arange(0,11), labels=['{:1.0f}'.format(s) for s in np.arange(0,11)])
-    ax.text(9100, -9, '$\\times 10^3$', fontdict=None, size=label_font_size)
-
-    ax.set_xlim([-250,10000])
-    ax.set_ylim([0,65])
-
-    ax.set_xlabel('Time-slot')
-    ax.set_ylabel('Total queue backlog')
-
-    ax.legend(loc = 'lower right')
-    ax.grid()
-
-# function to plot total transmission costs
-def plot_transmission_cost_curve(ax, unknownT_tran_cost_at_tt, knownT_tran_cost_at_tt, oracle_tran_cost_at_tt, label_font_size):
-    ax.plot(windowed_average(oracle_tran_cost_at_tt), color = 'C2', label = 'Oracle policy')
-    ax.plot(windowed_average(knownT_tran_cost_at_tt), color = 'C1', label = 'DPOP (given T)')
-    ax.plot(windowed_average(unknownT_tran_cost_at_tt), color = 'C0', label = 'DPOP (doubling)')
-
-    # show values in scientific notation and show exponent near axes
-    ax.set_xticks(ticks=1000*np.arange(0,11), labels=['{:1.0f}'.format(s) for s in np.arange(0,11)])
-    ax.text(9100, -0.7, '$\\times 10^3$', fontdict=None, size=label_font_size)
-
-    ax.set_xlim([-250,10000])
-    ax.set_ylim([0,5])
-
-    ax.set_xlabel('Time-slot')
-    ax.set_ylabel('Instantaneous transmission cost')
-
-    handles, labels = ax.get_legend_handles_labels()
-    order = [0,1,2]
-    order.reverse()
-    ax.legend([handles[idx] for idx in order],[labels[idx] for idx in order], loc = 'lower right')
-
     ax.grid()
